600dc0ca6c75498256a9e98ad5581c8f1d28fd76
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-07-17 14:17:00 -0400

    fixing logo

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../cssPages/images/finalcatchlogo.png';
+import logo from '../../cssPages/ImageGallery/finalcatchlogo';
 import '../Navigation/style.css';
 import About from '../../pages/About/About';
 
