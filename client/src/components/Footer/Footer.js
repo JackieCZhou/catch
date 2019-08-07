5bbabd0253ded66296fd17a8ad65f6527608e7df
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-08-07 13:46:00 -0400

    resizing footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -26,14 +26,14 @@ class Footer extends Component {
                 <hr class="clearfix w-100 d-md-none pb-3" />
           
           
-                <div class="col-md-3 mb-md-0 mb-3">
+                <div class="col-md-4 mb-md-0 mb-4">
           
                   <ul class="list-unstyled">
                     <li>
-                      <a href="https://jackieczhou.github.io/catch/"><i class="fab fa-github"></i> Repository</a>
-                      <a href="#!"><i class="fab fa-facebook"></i> Facebook</a>
-                      <a href="#!"><i class="fab fa-twitter-square"></i> Twitter</a>
-                      <a href="#!"><i class="fab fa-instagram"></i> Instagram</a>
+                      <a href="https://jackieczhou.github.io/catch/"><i class="fab fa-github"></i> Repository </a>
+                      <a href="#!"><i class="fab fa-facebook"></i> Facebook </a>
+                      <a href="#!"><i class="fab fa-twitter-square"></i> Twitter </a>
+                      <a href="#!"><i class="fab fa-instagram"></i> Instagram </a>
                     </li>
                   </ul>
           
