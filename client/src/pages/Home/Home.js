4895fcd8c5d4950f0a8bb9c5f19f4a77424f6061
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-08-07 11:16:38 -0400

    title grid

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -49,9 +49,9 @@ export default class Test extends React.Component {
           <div className="col-12">
             <h1 style={{ background: 'transparent', color: 'orangered', fontStyle: 'bold' }}>Catch</h1>
             <br />
+            <h4 style={{ color: 'navy', fontStyle: 'bold' }}>The new wave in job hunting and recruiting</h4>
+            < br ></br >
           </div>
-          <h4 style={{ color: 'navy', fontStyle: 'bold' }}>The new wave in job hunting and recruiting</h4>
-          < br ></br >
         </div>
 
 
@@ -138,14 +138,14 @@ export default class Test extends React.Component {
                         </div>
                       </div>
                     </div>
-                    {/* <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                       <span className="sr-only">Previous</span>
                     </a>
                     <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                       <span className="carousel-control-next-icon" aria-hidden="true"></span>
                       <span className="sr-only">Next</span>
-                    </a> */}
+                    </a>
                   </div>
                 </div>
                 <div classNameName="col-2"></div>
