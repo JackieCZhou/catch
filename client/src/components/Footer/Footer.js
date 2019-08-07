7af584a7fc07012c52f1ebb4fd8031b7fc772dc8
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-08-07 14:12:08 -0400

    resizing footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,55 +6,56 @@ import "./style.css"
 // import "./Confirmation"
 
 class Footer extends Component {
-    render() {
-        return (
-         <div>
-           <br />
-          <footer class="page-footer font-small pt-4">
-          
-            <div class="container-fluid text-center text-md-center">
-          
-              <div class="row">
-          
-                <div class="col-md-10 mt-md-0 mt-3">
-          
-                  <h5 class="text-uppercase">Catch</h5>
-                  <p>The ultimate in hiring and job hunting, never miss the perfect opportunity again!</p>
-          
-                </div>
-          
-                <hr class="clearfix w-100 d-md-none pb-3" />
-          
-          
-                <div class="col-md-2 mb-md-0 mb-2">
-          
-                  <ul class="list-unstyled" style={{textAlign: 'right'}}>
-                    <li>
-                      <a href="https://jackieczhou.github.io/catch/"><i class="fab fa-github"></i> Repository </a>
-                      <a href="#!"><i class="fab fa-facebook"></i> Facebook </a>
-                      <a href="#!"><i class="fab fa-twitter-square"></i> Twitter </a>
-                      <a href="#!"><i class="fab fa-instagram"></i> Instagram </a>
-                    </li>
-                  </ul>
-          
-                </div>
-          
+  render() {
+    return (
+      <div>
+        <br />
+        <footer class="page-footer font-small pt-4">
+
+          <div class="container-fluid text-center text-md-center">
+
+            <div class="row">
+
+              <div class="col-md-12 mt-md-0 mt-3">
+
+                <h5 class="text-uppercase">Catch</h5>
+                <p>The ultimate in hiring and job hunting, never miss the perfect opportunity again!</p>
+
               </div>
-          
-          
+
+              <hr class="clearfix w-100 d-md-none pb-3" />
+
             </div>
-            
-            <div class="footer-copyright text-center py-3">About 
-            <Link className='' to='/about' onClick={this.toggleCollapse}> Catch and </Link>  <a href="https://github.com/JackieCZhou">Jackie Zhou</a>
+            <div class="row">
+              <div class="col-md-12 mb-md-0 mb-12">
+
+                <ul class="list-unstyled" style={{ textAlign: 'right' }}>
+                  <li>
+                    <a href="https://jackieczhou.github.io/catch/"><i class="fab fa-github"></i> Repository </a>
+                    <a href="#!"><i class="fab fa-facebook"></i> Facebook </a>
+                    <a href="#!"><i class="fab fa-twitter-square"></i> Twitter </a>
+                    <a href="#!"><i class="fab fa-instagram"></i> Instagram </a>
+                  </li>
+                </ul>
+
+              </div>
+
             </div>
-          
-          </footer>
-          
+
+
           </div>
-  
+
+          <div class="footer-copyright text-center py-3">About
+            <Link className='' to='/about' onClick={this.toggleCollapse}> Catch and </Link>  <a href="https://github.com/JackieCZhou">Jackie Zhou</a>
+          </div>
+
+        </footer>
+
+      </div>
 
 
-        )
-    }
+
+    )
+  }
 }
 export default Footer;
