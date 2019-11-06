d9382fe4f35580c70703157e18ca64854676575f
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-11-06 10:22:55 -0500

    mongodb attach

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const app = express();
 //-- Mongoose Setup ----------------------------------------------------------
 mongoose.connect(
   process.env.MONGODB_URI ||
-  'mongodb://localhost/heroku_gnmt6k1t'
+  'mongodb://localhost/CatchApp'
 )
 mongoose.connection.on('error', err => {
   console.log(`Mongoose connection err:\n${err}`)
